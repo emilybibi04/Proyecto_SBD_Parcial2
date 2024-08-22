7c880c3b438b21f8cad723ef3c93e329676da48c
LuisVergaraA <luvearel@espol.edu.ec>
2024-08-22 01:05:25 -0500

    Creación
    Creación clase e interface empleado

--- src/main/java/com/mycompany/java_croud_mysql_proyecto/Principal.java
+++ src/main/java/com/mycompany/java_croud_mysql_proyecto/Principal.java
@@ -11,8 +11,10 @@ package com.mycompany.java_croud_mysql_proyecto;
 public class Principal {
 
     public static void main(String[] args) {
-        Interface_Persona objPersona = new Interface_Persona();
-        objPersona.setVisible(true);
+        //Interface_Persona objPersona = new Interface_Persona();
+        //objPersona.setVisible(true);
+        Interface_Empleado objEmpleado = new Interface_Empleado();
+        objEmpleado.setVisible(true);
         
     }
 }
